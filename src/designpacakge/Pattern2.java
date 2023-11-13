32e52fc695e0f6ec6a2af78d3d75976d7679df6e
chethanavirajini <cheth.virajini@gmail.com>
2023-11-13 16:10:20 +0530

    patterns

--- src/designpacakge/Pattern2.java
+++ src/designpacakge/Pattern2.java
@@ -1,14 +1,12 @@
 package designpacakge;
 
-public class Pattern1 {
+public class Pattern2 {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            for(int j=0; j < 5; j++){
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-
-
 }
