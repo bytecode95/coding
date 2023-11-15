c5ce0bf3e2bc19455b8880edb963cfb7b5b7e22f
chethanavirajini <cheth.virajini@gmail.com>
2023-11-15 12:53:45 +0530

    custom array

--- src/datastructure/Arraylistdemo.java
+++ src/datastructure/Arraylistdemo.java
@@ -1,6 +1,7 @@
 package datastructure;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Arraylistdemo {
     private int[] data;
@@ -25,18 +26,55 @@ public class Arraylistdemo {
         for (int i = 0; i < data.length; i++) {
             temp[i] = data[i];
         }
+        data = temp;
     }
 
     private boolean isFull() {
         return size == data.length;
     }
 
+    public int remove(){
+        int removed = data[size];
+        size--;
+        return removed;
+    }
+
+    public int get(int index){
+        return data[index];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void set(int index, int value){
+         data[index] = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Arraylistdemo{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
+    }
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
+        Arraylistdemo listdemo = new Arraylistdemo();
+        listdemo.add(3);
+        listdemo.add(5);
+        listdemo.add(9);
+        listdemo.add(12);
+        System.out.println((listdemo));
 
+        listdemo.remove();
+        System.out.println((listdemo));
 
-
+        for (int i = 0; i < 15; i++) {
+            listdemo.add(2*i);
+        }
+        System.out.println((listdemo));
 //        list.add(5);
 //        list.add(15);
 //        list.add(13);
